50233a7e82482f808dbd2c223737171e8be62df8
Tarkan74 <tarkan.selam@etu.univ-savoie.fr>
2013-11-12 10:22:57 +0100

    Premier test (Initialisation de la grille)

---
+++ KataReversi/src/Reversi/Reversi.java
@@ -0,0 +1,18 @@
+package Reversi;
+
+public class Reversi {
+
+	public String printLegalMoves(String input) {
+		return ""+
+				"........\n"
+				+ "........\n"
+				+ "....0...\n"
+				+ "...BW0..\n"
+				+ "..0WB...\n"
+				+ "...0....\n"
+				+ "........\n"
+				+ "........\n"
+				+ "B";
+	}
+
+}
--- KataReversi/src/Reversi/ReversiTest.java
+++ KataReversi/src/Reversi/ReversiTest.java
@@ -7,8 +7,31 @@ import org.junit.Test;
 public class ReversiTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void grilleAuCommencement(){
+		String input = ""+
+				"........\n"
+				+ "........\n"
+				+ "........\n"
+				+ "...BW...\n"
+				+ "...WB...\n"
+				+ "........\n"
+				+ "........\n"
+				+ "........\n"
+				+ "B";
+		
+		String output = ""+
+				"........\n"
+				+ "........\n"
+				+ "....0...\n"
+				+ "...BW0..\n"
+				+ "..0WB...\n"
+				+ "...0....\n"
+				+ "........\n"
+				+ "........\n"
+				+ "B";
+		
+		Reversi reversi = new Reversi();
+		assertEquals(output,reversi.printLegalMoves(input));
 	}
 
 }
