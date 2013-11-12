a08d5fe37c73e488a8d339698d2bf7568773199c
Tarkan74 <tarkan.selam@etu.univ-savoie.fr>
2013-11-12 09:53:13 +0100

    Création du projet

---
+++ KataReversi/src/Reversi/ReversiTest.java
@@ -0,0 +1,14 @@
+package Reversi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReversiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
